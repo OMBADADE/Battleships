f75f0c8c8c9e6a09e38d9ca0f5ae1c0cc0dca2fb
OMBADADE <ombadade27@gmail.com>
2024-05-13 20:03:13 -0500

    Flutter Project

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
     ChangeNotifierProvider(
       create: (_) => CommonValuesProvider(),
       child: const MaterialApp(
-        debugShowCheckedModeBanner: true,
+        debugShowCheckedModeBanner: false,
         title: 'Battleships',
         home: LoginPage(),
       ),
